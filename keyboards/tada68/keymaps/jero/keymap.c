93c4789a01dbd38c417716b09332429ffd473285
Jero Sutlovic <jsutlovic@gmail.com>
2019-09-19 23:55:47 +0200

    Game Layer
    - Combo chording of L/R WIN keys to switch layers
    - CAPS LED is lit when GAME mode is on

--- keyboards/tada68/keymaps/jero/keymap.c
+++ keyboards/tada68/keymaps/jero/keymap.c
@@ -6,6 +6,9 @@
 // entirely and just use numbers.
 #define _BL 0
 #define _FL 1
+#define _GAME 2
+
+#define SPCFN LT(_FL, KC_SPC)
 
 // Macro defns
 enum {
@@ -36,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,   KC_BSPC, KC_ESC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
   KC_LCTL,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,        KC_ENT,  KC_PENT, \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT,KC_SLSH,    KC_RSFT,KC_UP,  KC_MPLY, \
-  KC_LCTL, KC_LGUI,KC_LALT,              LT(_FL, KC_SPC),                   KC_RALT,KC_RGUI,KC_RCTRL,KC_LEFT,KC_DOWN, KC_RGHT),
+  KC_LSFT,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,     KC_RSFT,KC_UP,  KC_MPLY, \
+  KC_CAPS, KC_LGUI,KC_LALT,                     SPCFN,                      KC_RALT,KC_RGUI,KC_RCTRL,KC_LEFT,KC_DOWN, KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |        |   |   |Bl-|BL |BL+|Spc|MUT|VO-|VO+|   |     |Cycl|    |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |   |Prev|   |Next|
+   * |RESET|    |    |                      |   |   |   |Prev|   |Next|
    * `----------------------------------------------------------------'
    */
 [_FL] = LAYOUT_ansi(
@@ -57,7 +60,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,_______,_______, _______,_______,SP_PREV,TB_PREV,TB_NEXT,SP_NEXT,_______,_______, _______,KC_INS, \
   _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,_______,        _______,KC_END, \
   _______,_______,_______,BL_DEC, BL_TOGG,BL_INC, KC_SPC,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______, OS_CYCL, _______, \
-  _______,_______,_______,                 _______,               _______,_______,_______,KC_MPRV,OS_PRNTL,KC_MNXT),
+    RESET,_______,_______,                 _______,               _______,_______,_______,KC_MPRV,OS_PRNTL,KC_MNXT),
+
+  /* Keymap _GAME: (Base Layer) Game Layer - no SpaceFn
+   * ,----------------------------------------------------------------.
+   * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Esc |
+   * |----------------------------------------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
+   * |----------------------------------------------------------------|
+   * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |Ent |
+   * |----------------------------------------------------------------|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|Play|
+   * |----------------------------------------------------------------|
+   * |Ctrl|Win |Alt |        Space          |Alt|Win|Ctrl|Lef|Dow|Rig |
+   * `----------------------------------------------------------------'
+   */
+[_GAME] = LAYOUT_ansi(
+  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,   KC_BSPC, KC_ESC, \
+  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
+  KC_LCTL,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,        KC_ENT,  KC_PENT, \
+  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT,KC_SLSH,    KC_RSFT,KC_UP,  KC_MPLY, \
+  KC_LCTL, KC_LGUI,KC_LALT,                      KC_SPC,                    KC_RALT,KC_RGUI,KC_RCTRL,KC_LEFT,KC_DOWN, KC_RGHT),
+
 };
 
 
@@ -102,6 +126,34 @@ void flash_backlight(void) {
     wait_ms(OS_SWITCH_FLASH_DELAY);
     backlight_toggle();
 }
+
+// Combos
+enum combo_events {
+    BL_SWITCH,
+};
+
+const uint16_t PROGMEM switch_combo[] = {KC_LGUI, KC_RGUI, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    [BL_SWITCH] = COMBO_ACTION(switch_combo),
+};
+
+void process_combo_event(uint8_t combo_index, bool pressed) {
+    if (!pressed) return;
+    switch(combo_index) {
+        case BL_SWITCH:
+            if (layer_state_is(_GAME)) {
+                layer_clear();
+                PORTB |= (1<<2);
+            } else {
+                layer_move(_GAME);
+                PORTB &= ~(1<<2);
+            }
+            flash_backlight();
+            break;
+    }
+}
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!record->event.pressed) return true;
