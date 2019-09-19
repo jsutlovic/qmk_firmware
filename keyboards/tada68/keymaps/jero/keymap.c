0d71b1becb4ea190a58c6773f0f2f97734547755
Jero Sutlovic <jsutlovic@gmail.com>
2019-09-20 00:25:41 +0200

    RESET doesn't work

--- keyboards/tada68/keymaps/jero/keymap.c
+++ keyboards/tada68/keymaps/jero/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
   KC_LCTL,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,        KC_ENT,  KC_PENT, \
   KC_LSFT,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,     KC_RSFT,KC_UP,  KC_MPLY, \
-  KC_CAPS, KC_LGUI,KC_LALT,                     SPCFN,                      KC_RALT,KC_RGUI,KC_RCTRL,KC_LEFT,KC_DOWN, KC_RGHT),
+  KC_LCTL, KC_LGUI,KC_LALT,                     SPCFN,                      KC_RALT,KC_RGUI,KC_RCTRL,KC_LEFT,KC_DOWN, KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,_______,_______, _______,_______,SP_PREV,TB_PREV,TB_NEXT,SP_NEXT,_______,_______, _______,KC_INS, \
   _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,_______,        _______,KC_END, \
   _______,_______,_______,BL_DEC, BL_TOGG,BL_INC, KC_SPC,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______, OS_CYCL, _______, \
-    RESET,_______,_______,                 _______,               _______,_______,_______,KC_MPRV,OS_PRNTL,KC_MNXT),
+  _______,_______,_______,                 _______,               _______,_______,_______,KC_MPRV,OS_PRNTL,KC_MNXT),
 
   /* Keymap _GAME: (Base Layer) Game Layer - no SpaceFn
    * ,----------------------------------------------------------------.
